7ec46a1027297182f5ed51804e6d103adaba6120
zhuzi <1104589420@qq.com>
2019-03-09 15:41:37 +0800

    init

---
+++ src/test/java/com/zhuzi/example/mybatis/bean/TestBean.java
@@ -0,0 +1,44 @@
+package com.zhuzi.example.mybatis.bean;
+
+import java.util.Date;
+
+import com.zhuzi.mybatis.annotation.GeneratedValue;
+import com.zhuzi.mybatis.annotation.TableName;
+
+@TableName(name="test")
+public class TestBean {
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String testName;
+	private Date createTime;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTestName() {
+		return testName;
+	}
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	@Override
+	public String toString() {
+		return "TestBean [id=" + id + ", name=" + name + ", testName=" + testName + ", createTime=" + createTime + "]";
+	}
+	
+}
---
+++ src/test/java/com/zhuzi/example/mybatis/test/TemplateTest.java
@@ -0,0 +1,110 @@
+package com.zhuzi.example.mybatis.test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.google.common.collect.Lists;
+import com.zhuzi.ApplicationTests;
+import com.zhuzi.example.mybatis.bean.TestBean;
+import com.zhuzi.mybatis.core.query.Criteria;
+import com.zhuzi.mybatis.core.query.Query;
+import com.zhuzi.mybatis.template.MybatisTemplate;
+
+public class TemplateTest extends ApplicationTests{
+
+	@Autowired
+	private MybatisTemplate template;
+	
+	@Test
+	@Transactional
+	public void insertOne() {
+		TestBean bean = new TestBean();
+		bean.setName("name");
+		bean.setTestName("testName");
+		bean.setCreateTime(new Date());
+		assertTrue(template.insert(bean));
+		assertNotNull(bean.getId());
+	}
+	
+	@Test
+	@Transactional
+	public void update() {
+		TestBean bean = new TestBean();
+		bean.setName("name");
+		bean.setId(1);
+		assertNotEquals(template.update(bean), 0);
+	}
+	
+	@Test
+	@Transactional
+	public void delete() {
+		TestBean bean = new TestBean();
+		bean.setName("name");
+		bean.setId(1);
+		assertNotEquals(template.delete(bean), 0);
+	}
+	
+	@Test
+	public void selectOne() {
+		TestBean bean = template.selectOne(TestBean.class);
+		assertNotNull(bean);
+		assertNotNull(bean.getId());
+	}
+	
+	@Test
+	public void selectIn() {
+		Query query = new Query();
+		List<Integer> list = Lists.newArrayList();
+		list.add(0);
+		list.add(1);
+		query.addCriteria(Criteria.in("id", list));
+		List<TestBean> beans = template.select(query.getWhereMap(), TestBean.class);
+		assertNotNull(beans);
+	}
+	
+	@Test
+	public void selectEqual() {
+		Query query = new Query();
+		query.addCriteria(Criteria.equal("id", 1));
+		query.addCriteria(Criteria.equal("name", "name"));
+		List<TestBean> beans = template.select(query.getWhereMap(), TestBean.class);
+		assertNotNull(beans);
+	}
+	
+	@Test
+	public void selectNotEqual() {
+		Query query = new Query();
+		query.addCriteria(Criteria.notEqual("id", 1));
+		List<TestBean> beans = template.select(query.getWhereMap(), TestBean.class);
+		assertNotNull(beans);
+	}
+	
+	@Test
+	public void selectRightLike() {
+		Query query = new Query();
+		query.addCriteria(Criteria.rightLike("test_name", "test"));
+		List<TestBean> beans = template.select(query.getWhereMap(), TestBean.class);
+		assertNotNull(beans);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void selectBetween() {
+		Query query = new Query();
+		query.addCriteria(Criteria.between("create_time", new Date(118, 01, 01), new Date(120, 01, 01)));
+		Map<String, Object> map = query.getWhereMap();
+		List<TestBean> beans = template.select(map, TestBean.class);
+		System.out.println(beans);
+		assertNotNull(beans);
+	}
+	
+}
