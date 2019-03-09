44c2f28a9a0e5845ed861a0016054440f5dc7bab
zhuzi <1104589420@qq.com>
2019-03-09 16:30:45 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+示例说明
+==
+本工程是一个标准的spring boot工程，配置信息在src/main/resources/application.yml文件中
+运行示例在src/test/java文件夹的TemplateTest
+
+运行说明
+==
+* 将本工程和[mybatis-util](https://github.com/zhuzihuaile/mybatis-util)项目直接导入到IDE中,
+* 运行前需要修改src/main/resources/application.yml文件,将其中涉及到 MySQL 配置相关的部分改成自己的配置
+* 执行/src/test/resources/example.sql
+* 示例导入到IDE中后可以打开com.zhuzi.example.mybatis.test.TemplateTest类,这是一个测试类,可以直接执行测试类
---
+++ src/test/resources/example.sql
@@ -0,0 +1,27 @@
+/*
+Navicat MySQL Data Transfer
+
+Target Server Type    : MYSQL
+Target Server Version : 80015
+File Encoding         : 65001
+
+Date: 2019-03-09 16:07:12
+*/
+
+
+-- ----------------------------
+-- Table structure for `test`
+-- ----------------------------
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(24) DEFAULT NULL,
+  `test_name` varchar(24) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of test
+-- ----------------------------
+INSERT INTO `test` VALUES ('1', 'name', 'testName', '2018-10-01 10:00:00');
